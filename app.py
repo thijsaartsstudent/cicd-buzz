34a87218a02c6e784731c2913f479b647d0e698e
thijsaarts <thijs.aarts@student.hu.nl>
2020-12-26 19:29:35 +0000

    final update

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from buzz import generator
 
 app = Flask(__name__)
 
+
 @app.route("/")
-
-
 def generate_buzz():
     page = '<html><body><h1>'
     page += generator.generate_buzz()
@@ -14,7 +13,7 @@ def generate_buzz():
     page += '</h1></body></html>'
     return page
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
-
-    
+ 
